db5df4972f7251769bc85a582bed377967af90b8
kontseus <konceus1@gmail.com>
2022-07-16 18:36:37 +0300

    [hw-19]Laravel|Users & Roles config

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db' => [
+        'roles' => [
+            'admin'     => 'Admin',
+            'customer'  => 'Customer'
+        ]
+    ],
+];
---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class  RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = collect(config('constants.db.roles'));
+        $roles->each(fn($role) => Role::firstOrCreate(['name' => $role]));
+    }
+}
